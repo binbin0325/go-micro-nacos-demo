f7ad22d6347f27131f1dc5fcf8641a9d4dbe67d1
binbin.zhang <binbin.zhang@openjawtech.com>
2020-08-12 17:49:04 +0800

    commit

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"github.com/micro/go-micro"
-	_ "github.com/micro/go-plugins/broker/rabbitmq"
-	_ "github.com/micro/go-plugins/registry/kubernetes"
-	_ "github.com/micro/go-plugins/transport/nats"
+	"context"
+	helloworld "go-micro-demo/proto"
+	"nacos"
+
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/registry"
 )
 
+type Helloworld struct{}
+
+// Call is a single request handler called via client.Call or the generated client code
+func (e *Helloworld) Hello(ctx context.Context, req *helloworld.HelloRequest, rsp *helloworld.HelloResponse) error {
+	logger.Info("Received Helloworld.Call request")
+	return nil
+}
 func main() {
+	addrs := make([]string, 1)
+	addrs[0] = "192.168.23.178:8848"
+	registry := nacos.NewRegistry(func(options *registry.Options) {
+		options.Addrs = addrs
+	}) //a default to using env vars for master API
+
 	service := micro.NewService(
 		// Set service name
 		micro.Name("my.service"),
+		// Set service registry
+		micro.Registry(registry),
 	)
-
-	// Parse CLI flags
 	service.Init()
+	helloworld.RegisterGreeterHandler(service.Server(), new(Helloworld))
+	service.Run()
 }
